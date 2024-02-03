02c01c905632a545f6cb5423ec1c08ceccb14fa3
iaSokolov <iasokolov@gmail.com>
2024-02-03 13:10:49 +0300

    Initialize Spring Boot

---
+++ src/test/kotlin/ru/iasokolov/demo/AppTests.kt
@@ -0,0 +1,12 @@
+package ru.iasokolov.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
