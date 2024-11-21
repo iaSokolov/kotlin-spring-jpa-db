53caa37d4e627cdc96af31c7710494289654d198
iaSokolov <iasokolov@gmail.com>
2024-11-21 15:55:40 +0300

    Add "All open" property

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,12 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs += "-Xjsr305=strict"
